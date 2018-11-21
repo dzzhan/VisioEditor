b150a64d0f4b7fb18d3ece7022c756b2c96b0c66
dzzhan <dzzhan@163.com>
2018-11-21 11:06:53 +0800

    add initial version

---
+++ VisioEditor/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace VisioEditor
+{
+    partial class GraphEditor
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.axDrawingControl1 = new AxMicrosoft.Office.Interop.VisOcx.AxDrawingControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.axDrawingControl1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axDrawingControl1
+            // 
+            this.axDrawingControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axDrawingControl1.Enabled = true;
+            this.axDrawingControl1.Location = new System.Drawing.Point(0, 25);
+            this.axDrawingControl1.Name = "axDrawingControl1";
+            this.axDrawingControl1.Size = new System.Drawing.Size(800, 507);
+            this.axDrawingControl1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(39, 21);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(42, 21);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // GraphEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.axDrawingControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GraphEditor";
+            this.Text = "GraphEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axDrawingControl1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private AxMicrosoft.Office.Interop.VisOcx.AxDrawingControl axDrawingControl1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ VisioEditor/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Visio = Microsoft.Office.Interop.Visio;
+using AxVisOcx = Microsoft.Office.Interop.VisOcx;
+//using System.Web.UI.WebControls;
+using Cell = Microsoft.Office.Interop.Visio.Cell;
+
+namespace VisioEditor
+{
+    public partial class GraphEditor : Form
+    {
+        public GraphEditor()
+        {
+            InitializeComponent();
+        }
+
+        private static void ConnectShapes(Visio.Shape shape1, Visio.Shape shape2, Visio.Shape connector)
+        {
+            // get the cell from the source side of the connector
+
+            Cell beginXCell = connector.get_CellsSRC(
+
+            (short)Visio.VisSectionIndices.visSectionObject,
+
+
+            (short)Visio.VisRowIndices.visRowXForm1D,
+
+            (short)Visio.VisCellIndices.vis1DBeginX);
+
+            // glue the source side of the connector to the first shape
+
+            beginXCell.GlueTo(shape1.get_CellsSRC(
+
+            (short)Visio.VisSectionIndices.visSectionObject,
+
+            (short)Visio.VisRowIndices.visRowXFormOut,
+
+            (short)Visio.VisCellIndices.visXFormPinX));
+
+            // get the cell from the destination side of the connector
+
+            Cell endXCell = connector.get_CellsSRC(
+
+            (short)Visio.VisSectionIndices.visSectionObject,
+
+            (short)Visio.VisRowIndices.visRowXForm1D,
+
+            (short)Visio.VisCellIndices.vis1DEndX);
+
+            // glue the destination side of the connector to the second shape
+
+            endXCell.GlueTo(shape2.get_CellsSRC(
+
+            (short)Visio.VisSectionIndices.visSectionObject,
+
+            (short)Visio.VisRowIndices.visRowXFormOut,
+
+            (short)Visio.VisCellIndices.visXFormPinX));
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axDrawingControl1.Window.Zoom = 0.5;
+            axDrawingControl1.Window.ShowScrollBars = (short)Visio.VisScrollbarStates.visScrollBarBoth;
+            axDrawingControl1.Window.ShowRulers = 0;
+            axDrawingControl1.Window.BackgroundColor = (uint)ColorTranslator.ToOle(Color.Red);
+            axDrawingControl1.Window.BackgroundColorGradient = (uint)ColorTranslator.ToOle(Color.Red);
+            axDrawingControl1.Window.ZoomBehavior = Visio.VisZoomBehavior.visZoomVisioExact;
+            Visio.Page currentPage = axDrawingControl1.Document.Pages[1];
+            Visio.Document currentStencil = axDrawingControl1.Document.Application.Documents.OpenEx("Basic_M.vss",
+                                                                        (short)Visio.VisOpenSaveArgs.visOpenDocked);
+            Visio.Document auditStencil = axDrawingControl1.Document.Application.Documents.OpenEx("AUDIT_M.VSSX",
+                                                                         (short)Visio.VisOpenSaveArgs.visOpenDocked);
+            Visio.Shape shape1 = currentPage.Drop(currentStencil.Masters["矩形"], 1.50, 1.50);
+            Visio.Shape shape2 = currentPage.Drop(currentStencil.Masters["正方形"], 2.50, 3.50);
+            Visio.Shape connector = currentPage.Drop(auditStencil.Masters["动态连接线"], 4.50, 4.50);
+            ConnectShapes(shape1, shape2, connector);
+            Cell arrowCell = connector.get_CellsSRC((short)Visio.VisSectionIndices.visSectionObject, (short)Visio.VisRowIndices.visRowLine, (short)Visio.VisCellIndices.visLineEndArrow);
+            arrowCell.FormulaU = "5";
+            connector.get_Cells("EndArrow").Formula = "=5";
+        }
+    }
+}
